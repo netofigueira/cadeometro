c5776586c9906a58b091a3b8891a92adcc918077
netofigueira <neto.gffn@gmail.com>
2023-02-01 15:12:20 -0300

    load quinto andar markers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
-import folium
+from src.buildings.build_lister_quinto_andar import BuildListerQuintoAndar
 
 app = Flask(__name__)
 
@@ -16,13 +16,10 @@ groups_df = pd.read_csv('preprocess/linhas_metro.csv')
 
 groups_df.dropna(inplace=True)
 groups = groups_df[['linha', 'linhaID']].drop_duplicates().to_dict('records')
-print(groups)
 #groups = [{'name': 'group 1', 'id': 1}, {'name': 'group 2', 'id': 2}, {'name': 'group 3', 'id': 3}]
 
 # Create a dictionary to store the locations for each group
 locations = groups_df.groupby('linhaID').apply(lambda x: list(zip(x['latitude'], x['longitude']))).reset_index().dropna()[0].to_dict()
-print(locations)
-
 @app.route("/")
 def index():
     return render_template("index.html", groups=groups)
@@ -35,18 +32,31 @@ def map():
     # Create a map centered on the first location of the selected group
     df = groups_df[groups_df.linhaID == int(group_id)]
     locs = list(zip(df['latitude'], df['longitude']))
-    map = folium.Map(location=locs[0], zoom_start=13)
+    lats = df.latitude.to_list()
+    lngs = df.longitude.to_list()
+    names = df.estacao.to_list()
+    return render_template("map.html", locs=locs, lats=lats, lngs =lngs, names=names)
 
-    # Add markers for each location in the selected group
-    for location in locs:
-        folium.Marker(location).add_to(map)
-        # Add a marker for each location
-        folium.Marker(location=location,
-                      popup=
-                      df.loc[(df['latitude'] == location[0]) & (df['longitude'] == location[1]), 'estacao'].values[
-                          0]).add_to(map)
 
-    return render_template("map.html", map=map._repr_html_())
+@app.route("/get_quinto_andar_data", methods=['GET'])
+def get_quinto_andar():
 
+
+    lat = float(request.args.get('lat'))
+    lng = float(request.args.get('lng'))
+    builder = BuildListerQuintoAndar()
+    buildings_close = builder.get_buildings_close(lat, lng)
+
+    tresh = min(10, len(buildings_close))
+
+    # filtrar 10 elementos por enquanto, pq estão carregando mtos
+    # lidar com isso dps
+    buildings_close = buildings_close[:tresh]
+    return jsonify({
+        'buildings': [
+            {'lat': building.lat, 'lng': building.lon, 'building_id': building._id}
+            for building in buildings_close
+        ]
+    })
 if __name__ == "__main__":
     app.run(debug=True)
