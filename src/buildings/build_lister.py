43f463f89c627a58cb1c64238d093e88aa9f5515
Eduardo Vicentini <edudiasvicentini@hotmail.com>
2024-05-28 18:57:11 -0300

    feaure: filter price on lister

--- src/buildings/build_lister.py
+++ src/buildings/build_lister.py
@@ -10,14 +10,17 @@ class BuildLister:
         raise NotImplementedError
 
     def get_buildings_close(
-        self, lat: str, lon: str, distance: float = 1.0
+        self,
+        lat: str,
+        lon: str,
+        distance: float = 1.0,
+        min_value: float = 100,
+        max_value: float = 100000,
     ) -> list:
-        bounds = self._bounding_box(lat, lon)
-        return self.get_building_list(*bounds)
+        bounds = self._bounding_box(lat, lon, distance=distance)
+        return self.get_building_list(lat, lon, *bounds, min_value, max_value)
 
-    def _bounding_box(
-        self, lat: str, lon: str, distance: float = 1.0
-    ) -> Tuple[int]:
+    def _bounding_box(self, lat: str, lon: str, distance: float = 1.0) -> Tuple[int]:
         # Earth's radius in kilometers
         R = 6371.0
 
